e60f0d19a2b1393fff8db59be1f21d53f7705bc1
Laura Kotalczyk <laura.kotalczyk@yahoo.de>
2025-12-20 20:00:36 +0100

    added first implementation of goals-met-screen

--- nutri_ai/constants/values.ts
+++ nutri_ai/constants/values.ts
@@ -3,6 +3,7 @@ import { Platform } from "react-native";
 // Constants
 export const MS_TO_S: number = 1_000;
 export const DAILY_CALORIE_GOAL = 2500;
+export const MIN_LOGGING_THRESHOLD = 1500;
 
 // API Configuration
 export const API_BASE_URL = `https://pangolin.7cc.xyz:19855`;
